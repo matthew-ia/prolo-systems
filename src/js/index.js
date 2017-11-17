b0b308359c06476a8ca983d4d9a67ef50afc3e60
braxtonCoats <coatsbr@appstate.edu>
2017-11-17 13:08:20 -0500

    braxtons new index and js

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+/* Function to create tables for each section */
+function generalTable() {
+  $('#general').append('<form></form>');
+  $('#general form').append('<input type="text" name="reportName" placeholder="Report Name">');
+  $('#general form').append('<div id="general-info" class="sub-section">');
+  $('#general-info').append('<h2>General Info</h2>');
+  $('#general-info').append('<input type="text" name="logNumber" placeholder="Log #">');
+  $('#general-info').append('<input type="text" name="date" placeholder="Date">');
+  $('#general-info').append('<input type="text" name="ownerName" placeholder="Owner Name">');
+  $('#general-info').append('<textarea name="businessAddress" placeholder="Business Address"></textarea></div>');
+
+  $('#general form').append('<div id="audit-contact" class="sub-section">');
+  $('#audit-contact').append('<h2>Audit Contact</h2>');
+  $('#audit-contact').append('<input type="text" name="contactName" placeholder="Contact Name">');
+  $('#audit-contact').append('<input type="text" name="contactPhoneNumber" placeholder="Contact Phone #">');
+  $('#audit-contact').append('<textarea name="contactAddress" placeholder="Contact Address"></textarea></div');
+
+
+}
+
+$("#content-container").append(generalTable());
+/*  $("#share").append('<form action="sharer.php" method="POST">');
+   $("#share form").append('<div class="appm">Save this</div>');
+   $("#share form").append('<input type="text" placeholder="Name" name="routename" id="rname"/>');
+   $("#share form").append('<input type="text" placeholder="description" id="rdescription" name="routedescription" class="address"/>');
+   $("#share form").append('<input type="text" placeholder="tags" id="tags" name="routetags"/>');
+   $("#share form").append('<br><input type="submit" id="savebutton" value="*/
