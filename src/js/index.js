ec66350bf8c7405f4870fd0e776134f0e13e7c75
Matthew Alicea <aliceami@appstate.edu>
2017-11-25 20:35:50 -0500

    Cleaned up styles
    - added appropriate widths to each table column
    - fixed some color issues
    - added the datepicker plugin & styled it

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,8 @@ function generalTable() {
   $('#general form').append('<div id="general-info" class="sub-section">');
   $('#general-info').append('<h2>General Info</h2>');
   $('#general-info').append('<input type="text" name="logNumber" placeholder="Log #">');
-  $('#general-info').append('<input type="text" name="date" placeholder="Date">');
+  $('#general-info').append('<input data-toggle="datepicker" type="text" name="date" placeholder="Date">');
+  //$('#general-info').append('<div id="date" data-toggle="datepicker"></div>');
   $('#general-info').append('<input type="text" name="ownerName" placeholder="Owner Name">');
   $('#general-info').append('<textarea name="businessAddress" placeholder="Business Address"></textarea></div>');
 
@@ -27,3 +28,9 @@ $("#content-container").append(generalTable());
    $("#share form").append('<input type="text" placeholder="description" id="rdescription" name="routedescription" class="address"/>');
    $("#share form").append('<input type="text" placeholder="tags" id="tags" name="routetags"/>');
    $("#share form").append('<br><input type="submit" id="savebutton" value="*/
+
+
+
+
+/* DATE PICKER */
+$('[data-toggle="datepicker"]').datepicker();
