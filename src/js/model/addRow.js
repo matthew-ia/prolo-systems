e7bcba28ba6de36cd17f55c8daa4d828c6b676de
Matthew Alicea <aliceami@appstate.edu>
2017-12-05 12:39:41 -0500

    Undid the combination of enableAddRow and addRow, but changed their implementations a bit

--- src/js/model/addRow.js
+++ src/js/model/addRow.js
@@ -1,13 +1,17 @@
 
 // Add new row with increminted row ID
 function enableAddRow() {
+    // Attach listener to buttons once report is loaded
     $('.addRow button').click(function (){
-
-      var sectionId = $(this).parents('.section').attr('id');
-      var rowId = $('#' + sectionId).find('tbody tr:last-of-type').attr('id');
-      rowId = rowId.charAt(2);
-      var newId = parseInt(rowId) + 1;
-      var rowData = $('#' + sectionId + ' table tr:last').html();
-      $('#' + sectionId + ' table').append('<tr id="1-' + newId + '">'+ rowData + '</tr>');
+        addRow(true, $(this));
     });
-};
+}
+
+function addRow(isReportLoaded, sectionElem) {
+    var sectionId = sectionElem.parents('.section').attr('id');
+    var rowId = $('#' + sectionId).find('tbody tr:last-of-type').attr('id');
+    rowId = rowId.charAt(2);
+    var newId = parseInt(rowId) + 1;
+    var rowData = $('#' + sectionId + ' table tr:last').html();
+    $('#' + sectionId + ' table').append('<tr id="1-' + newId + '">'+ rowData + '</tr>');
+}
