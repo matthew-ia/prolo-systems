9722a846136a5cb331f0b345e211c895f01d6b4a
Finnican <finnicanmm@student.cs.appstate.edu>
2017-12-08 14:02:04 -0500

    added sectionc

--- src/js/model/save.js
+++ src/js/model/save.js
@@ -224,6 +224,42 @@ function enableAutoSave() {
         }
     });
 
+    // Schedule C section
+    $('#sectionc input, #sectionc select').blur(function() {
+            let parents = $(this).parents();
+            var rowIdSelected = undefined;
+            var rowIdLast;
+            var rowId;
+            for (i = 0; i < parents.length; i++) {
+            if($(parents[i]).is('tr')) {
+            rowIdSelected = $(parents[i]).attr('id');
+            rowIdLast = rowIdSelected.split("-").pop();
+            }
+            }
+            rowId = '#' + rowIdSelected;
+
+            switch($(this).attr('name')) {
+            case 'nameOwner':
+            REPORT.sectionc.items[rowIdLast].nameOwner = $(rowId + ' [name="nameOwner"]').val();
+            break;
+            case 'description':
+            REPORT.sectionc.items[rowIdLast].description = $(rowId + ' [name="description"]').val();
+            break;
+            case 'leaseNumber':
+            REPORT.sectionc.items[rowIdLast].leaseNumber = parseInt($(rowId + ' [name="leaseNumber"]').val());
+            break;
+            case 'monthlyPay':
+            REPORT.sectionc.items[rowIdLast].monthlyPay = parseInt($(rowId + ' [name="monthlyPay"]').val());
+            break;
+            case 'costNew':
+            REPORT.sectionc.items[rowIdLast].costNew = parseInt($(rowId + ' [name="costNew"]').val());
+            break;
+            case 'startEndDates':
+            REPORT.sectionc.items[rowIdLast].startEndDates = parseInt($(rowId + ' [name="startEndDates"]').val());
+            break;
+            }
+    });
+
 
     // TODO: Fix bug, still doesn't work
     /*
